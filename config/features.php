016080f695c1070b212eb6935f86211d8d2b4b34
OFFLINE <services@offline.ch>
2023-06-11 15:08:39 +0000

    v3.1.0-rc.2

--- config/features.php
+++ config/features.php
@@ -1,10 +1,5 @@
 <?php
 
 return [
-    /// PRO
-    'revisions' => (bool)env('BOXES_REVISIONS_ENABLED', true),
-    'multisite' => (bool)env('BOXES_MULTISITE_ENABLED', true),
-    'references' => (bool)env('BOXES_REFERENCES_ENABLED', true),
-    /// PRO
     'placeholderPreviews' => (bool)env('BOXES_PLACEHOLDER_PREVIEWS_ENABLED', true),
 ];
